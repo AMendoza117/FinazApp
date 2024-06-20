53c586b89fa78c7b47cc15114549088abd718ace
hopeosorio <ordonezosoriocesar@gmail.com>
2024-06-19 20:04:29 -0600

    cambios césar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { Component, NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router'
+import { LoginComponent } from './login/login.component';
+import { TokenComponent } from './token/token.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: '', component: TokenComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
