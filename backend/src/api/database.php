9977502b7195c6e381f79849cfb6a42f59cdabb0
Alan Mendoza <alanmendozaa117@gmail.com>
2024-07-03 11:58:06 -0600

    Registro de usuarios

--- backend/src/api/database.php
+++ backend/src/api/database.php
@@ -13,9 +13,9 @@ define('DB_PASS', '6mENK3R0vqeFDMimZVXw');
 define('DB_NAME', 'b5gpr0dt5vig4v4fuchq');/*
 
 define('DB_HOST', 'localhost'); 
-define('DB_USER', 'id22047545_gestordeproyectosutng');
-define('DB_PASS', 'ayerPrendiFuego1?');
-define('DB_NAME', 'id22047545_gestordeproyectosutng');*/
+define('DB_USER', 'root');
+define('DB_PASS', 'password');
+define('DB_NAME', 'finazapp');*/
 function connect()
 {
   $connect = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
